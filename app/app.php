255f2ccedc47b0f6322bea293615d7bf295ebc58
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-06-17 14:09:35 -0700

    Add initial Silex Web App

---
+++ app/app.php
@@ -0,0 +1,39 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get('/', function() use ($app) {
+        return 'To Do';
+    });
+
+    // Posted data route
+    // The following is not yet working
+    // It has been tested with:
+    // curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyzabc"}' http://localhost:8000/post
+
+    $app->post('/post', function() use($app) {
+        $input = file_get_contents('php://input');
+
+        // To Do:
+        // verify proper function on deployed site
+        // support authentication
+        // remove diagnostics
+
+        // echo '<pre>';
+        // echo print_r($input);
+        // echo '</pre>';
+
+        $output = '<pre>' . print_r($input, true) . '</pre>';
+        return $output;
+    });
+
+
+    return $app;
+?>
