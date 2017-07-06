25c4975bc36f8540b431c0f5a51fcaba0b2fca64
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-07-06 11:10:08 -0700

    Add tests

--- tests/SomePasswordTest.php
+++ tests/SomePasswordTest.php
@@ -17,7 +17,15 @@
             $passwords[] = '123Go!';
 
             $resources[] = 'it';
-            $users[] = 'them';
+            $users[] = 'works';
+            $passwords[] = 'Abracadabra';
+
+            $resources[] = 'it';
+            $users[] = 'cousin';
+            $passwords[] = 'abracadabra';
+
+            $resources[] = 'it';
+            $users[] = 'works';
             $passwords[] = 'abracadabra';
 
             $expected_results = array(
@@ -26,7 +34,28 @@
                     'user' => 'james',
                     'password' => 'asdf',
                     'result' => 400,
-                    'reasoning' => 'Unknown resource - 400 Bad Request)',
+                    'reasoning' => 'Unknown resource - 400 Bad Request',
+                ],
+                [
+                    'resource' => 'data',
+                    'user' => 'admin',
+                    'password' => '123Go!',
+                    'result' => 200,
+                    'reasoning' => 'Match! - 200 Good request',
+                ],
+                [
+                    'resource' => 'data',
+                    'user' => 'administrator',
+                    'password' => '123Go!',
+                    'result' => 401,
+                    'reasoning' => 'Unknown user - 401 Unauthorized',
+                ],
+                [
+                    'resource' => 'it',
+                    'user' => 'works',
+                    'password' => 'abra!',
+                    'result' => 403,
+                    'reasoning' => 'Bad password - 403 Forbidden',
                 ],
             );
 
