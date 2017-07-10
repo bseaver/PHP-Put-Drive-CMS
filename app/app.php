c729e1379239183d9f2443533be8d1b0fb2882e5
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-07-09 18:12:57 -0700

    Add caching for Google User Content images

--- app/app.php
+++ app/app.php
@@ -31,6 +31,8 @@
     $app->post('/data', function(Request $request) use($app) {
     })->before(function (Request $request) {
         require_once __DIR__.'/../src/SomePassword.php';
+        require_once __DIR__.'/../src/GoogleUserContentImage.php';
+        require_once __DIR__.'/../src/ImageFile.php';
 
         // Test (assuming resource=data, user=admin, password=password and sendfile.json exists at root)
         // curl -H "Authorization: Basic YWRtaW46cGFzc3dvcmQ=" -H "Content-Type: application/json" -X POST -d @sendfile.json localhost:8888/data
@@ -59,30 +61,39 @@
             $newFile = $file . '.new';
             $targetFile = $file . '.json';
             $oldFile = $file . '.old';
+            $tempFile = $file . '.tmp';
 
-            // Delete any "new" file
-            if ( file_exists($newFile) ) {
-                unlink($newFile);
+            // Delete any "tmp" file
+            if ( file_exists($tempFile) ) {
+                unlink($tempFile);
             }
-            if ( file_exists($newFile) ) {
+            if ( file_exists($tempFile) ) {
                 $statusCode = 551;
             }
 
-            // Save "new" file and verify valid json
+            // Save "tmp" file and verify valid json
             if ($statusCode === 200) {
-                file_put_contents($newFile, $input);
-                $contents = file_get_contents($newFile);
+                file_put_contents($tempFile, $input);
+                $contents = file_get_contents($tempFile);
                 $testData = json_decode($contents);
                 if (!$testData) {
                     $statusCode = 552;
                 }
             }
 
-            // Move previous "target" file to "old" file
-            if ($statusCode === 200 && file_exists($targetFile)) {
-                rename($targetFile, $oldFile);
-                if (file_exists($targetFile) || !file_exists($oldFile)) {
+            // Move previous "new" file to "old" file
+            if ($statusCode === 200 && file_exists($newFile)) {
+                rename($newFile, $oldFile);
+                if (file_exists($newFile) || !file_exists($oldFile)) {
                     $statusCode = 553;
+                }
+            }
+
+            // Move "tmp" file to "new" file
+            if ($statusCode === 200) {
+                rename($tempFile, $newFile);
+                if (file_exists($tempFile) || !file_exists($newFile)) {
+                    $statusCode = 554;
                 }
             }
 
@@ -90,7 +101,80 @@
             if ($statusCode === 200) {
                 copy($newFile, $targetFile);
                 if (!file_exists($newFile) || !file_exists($targetFile)) {
-                    $statusCode = 554;
+                    $statusCode = 555;
+                }
+            }
+
+            // Parse "new" file to separate out images
+            // and download them for local hosting
+            $imageFolder = __DIR__ . '/../web/cached_images/';
+            $runtimeImagePath = '/cached_images/';
+            $imageFilePrefix = $resource . '-img-';
+            $imageFileSuffix = '.jpeg';
+            $imageFileWildCard = $imageFolder . $imageFilePrefix . '*' . $imageFileSuffix;
+            $imgNumber = 0;
+            $processedInput = '';
+
+
+            // First clean out old downloaded images
+            if ($statusCode === 200) {
+                foreach(glob($imageFileWildCard) as $oldImageFile) {
+                    unlink($oldImageFile);
+                }
+
+                // Verify files are gone
+                foreach(glob($imageFileWildCard) as $oldImageFile) {
+                    $statusCode = 561;
+                    break;
+                }
+            }
+            
+            // Download any Google User Content images
+            if ($statusCode === 200) {
+                $filePieces = ImageFile::parseImgSrc($input);
+
+                // For each googleusercontent image
+                foreach ($filePieces as $filePiece) {
+                    if (GoogleUserContentImage::isGoogleUserContentImage($filePiece->imgURI)) {
+                        $imgNumber++;
+                        $newImageFile = $imageFilePrefix . $imgNumber . $imageFileSuffix;
+                        $newImageTarget = $imageFolder . $newImageFile;
+                        $newImagePath = $runtimeImagePath . $newImageFile;
+                        if (GoogleUserContentImage::downloadImg($filePiece->imgURI, $newImageTarget)) {
+                            $filePiece->imgURI = $newImagePath;
+                        } else {
+                            $statusCode = 562;
+                            break;
+                        }
+                    }
+                    $processedInput .= $filePiece->contents . $filePiece->imgURI;
+                }
+            }
+
+            // Verify processed input is ok
+            if ($statusCode === 200 && $imgNumber) {
+                file_put_contents($tempFile, $processedInput);
+                $contents = file_get_contents($tempFile);
+                $testData = json_decode($contents);
+                if (!$testData) {
+                    $statusCode = 563;
+                }
+            }
+
+            // Delete unprocessed "target"
+            if ($statusCode === 200 && $imgNumber) {
+                unlink($targetFile);
+                if (file_exists($targetFile)) {
+                    $statusCode = 564;
+                }
+            }
+
+            // Move processed file to "target"
+            if ($statusCode === 200 && $imgNumber) {
+                rename($tempFile, $targetFile);
+                if (file_exists($tempFile) || !file_exists($targetFile)) {
+                    $statusCode = 565;
+                    copy($newFile, $targetFile);
                 }
             }
 
